5ab2be07394a50e2edcaf2942867f5f2e8881874
Anick Sarkar <anicksarkar18032003@gmail.com>
2025-03-16 19:02:02 +0530

    conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,7 +21,53 @@ console.log(booleaIsLoggedIn)
 // "" (emptystring) => false
 // "any string" => true
 
+
 let someNumber =  33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber) //will show string
+
+
+// ****************************** OPERATIONS ****************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); //2^2
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " anick"
+console.log(str1 + str2);
+
+// ******* confusing section ******
+console.log( "1" + 2 ); //12
+console.log(1 + "2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); // 32
+
+
+console.log(+true); // converts true to number
+console.log(+""); // converts true to number
+
+let num1 , num2 , num3 ;
+num1 = num2 = num3 = 1
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+//read prefix postfix from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
