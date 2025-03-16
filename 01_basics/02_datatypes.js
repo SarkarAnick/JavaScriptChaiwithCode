94dab2483896fb5ac2bcf738b4b8a0676fdc7865
Anick Sarkar <anicksarkar18032003@gmail.com>
2025-03-16 17:35:33 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,5 +1,27 @@
 "use strict" ; // all code will be treated as newer version of javascript\
 
 //alert(3+3)// will show error as we are using node.js not browser
-//anick sakrar
-console.log("hello")
+console.log("anick")
+
+let name = "anick"
+let age = 21
+let isLoggedIn = false
+let nullSetter = null
+console.log(isLoggedIn)
+console.log(nullSetter)
+
+// number = 2^53
+//bigint
+//string => "" // preferrable to use within double quotes than single quotes
+//boolean => true/false
+// null => a stand-alone value / can be used as declaration of empty value
+console.log(nullSetter)
+// undefined => no value is present
+// symbols => to define something unique -- will be seen better in React.js
+
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null) // shows null is an object
+console.log(typeof undefined) // shows as undefined it self
