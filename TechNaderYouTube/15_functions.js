bd72668460ccae9a2cc1f622c3a407345c034e2e
Anick Sarkar <anicksarkar18032003@gmail.com>
2025-03-25 22:02:50 +0530

    adding youtube tech nader stuff mainly for clearer vision on functions and advanced stuff including oop

---
+++ TechNaderYouTube/15_functions.js
@@ -0,0 +1,8 @@
+// Regular functions --> 
+//                     function printHello(){
+//                         console.log("hello");
+//                     }
+// Arrow functions --> the newer way to declare a function as if we are declaring a variable
+//                   const printHello = () => {
+//                       console.log("hello");
+//                   }  
